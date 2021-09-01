d783337a4a54b1fdeebe2c9246982dc112447b3a
William Crews <crewswh@gmail.com>
2021-09-01 15:35:45 -0400

    models now

--- routes/path.js
+++ routes/path.js
@@ -5,4 +5,13 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
+
+
 module.exports = router;
