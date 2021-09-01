8865b222b74e069ee5aceff2b52633097a2c6ef5
William Crews <crewswh@gmail.com>
2021-09-01 15:32:36 -0400

    words

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-aspp.use(require("./routes/path"));
+app.use(require("./routes/path"));
 
 app.use(express.static("public"));
 
