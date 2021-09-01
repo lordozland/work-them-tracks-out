1f44a9043c6af07cab1ccd0180e53fe080a7a558
William Crews <crewswh@gmail.com>
2021-09-01 15:30:50 -0400

    continuous

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+aspp.use(require("./routes/path"));
 
 app.use(express.static("public"));
 
