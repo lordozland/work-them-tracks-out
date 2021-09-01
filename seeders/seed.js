6812b3654fcdd759b296f2f5fb4e154c53122451
William Crews <crewswh@gmail.com>
2021-09-01 15:41:30 -0400

    questions

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('../public/index.js');
+const db = require('../models');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
