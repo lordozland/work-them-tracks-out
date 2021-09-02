33da39bcfcc83e9ccd48886f2369a45384052e62
William Crews <crewswh@gmail.com>
2021-09-02 11:56:18 -0400

    heroku issues

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
