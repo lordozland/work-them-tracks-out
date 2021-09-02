e4f507777a9f6f59d862244950079f15c39df06c
William Crews <crewswh@gmail.com>
2021-09-02 10:37:55 -0400

    workouts db

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,16 @@ const Workouts = require("../models/workouts.js")
 
 router.get("/api/workouts", (req,res) => {
     Workouts.find()
+    .then()
 })
+
+Transaction.find({})
+.sort({ date: -1 })
+.then(dbTransaction => {
+  res.json(dbTransaction);
+})
+.catch(err => {
+  res.status(400).json(err);
+});
 
 module.exports = router;
