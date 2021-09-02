11f5d569284c44e1fab50883d81af95ecc27f608
William Crews <crewswh@gmail.com>
2021-09-02 11:49:25 -0400

    done-ish

--- routes/api.js
+++ routes/api.js
@@ -21,5 +21,15 @@ router.get("/api/workouts", (req,res) => {
       });
 })
 
+router.get("/api/workouts/range", (req,res) => {
+  Workouts.find({})
+  .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+})
+
 
 module.exports = router;
