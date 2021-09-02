cf45e32c1ff8f58bc38f3b20384bcd4e41891d50
William Crews <crewswh@gmail.com>
2021-09-02 10:31:31 -0400

    not routing

--- routes/html.js
+++ routes/html.js
@@ -5,4 +5,12 @@ router.get("/", (req, res) => {
     res.sendFile(path.join(_dirname, "../public/index.html"));
 });
 
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/stats.html"));
+});
+
 module.exports = router;
