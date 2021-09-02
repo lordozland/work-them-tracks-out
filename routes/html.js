8207020bc451574eaf8a226fd02d1498adfc7edc
William Crews <crewswh@gmail.com>
2021-09-02 10:22:08 -0400

    now it works

--- routes/html.js
+++ routes/html.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/", req, res => {
+router.get("/", (req, res) => {
     res.sendFile(path.join(_dirname, "../public/index.html"));
 });
