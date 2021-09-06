4372d4d45637bba6e03a777326dea8ed9193b70e
William Crews <crewswh@gmail.com>
2021-09-05 20:30:57 -0400

    words

--- routes/api.js
+++ routes/api.js
@@ -31,5 +31,21 @@ router.get("/api/workouts/range", (req,res) => {
     });
 })
 
+router.put("/api/workouts/:id", (req,res) => {
+  Workouts.findByIdAndUpdate(
+    { _id: req.patrams.id },
+    { $push: { excercises: req.body } },
+    {
+      new: true,
+      runValidators: true,
+    })
+  .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+})
+
 
 module.exports = router;
